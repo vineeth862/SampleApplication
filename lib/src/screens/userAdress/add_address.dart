504dfe7f6634cafe91f89ec4e4611f142fc15fb4
vineeth862 <vineethvini8888@gmail.com>
2023-07-12 09:04:23 +0530

    Created initial address and add address screem

---
+++ lib/src/screens/userAdress/add_address.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+
+class AddAdress extends StatelessWidget {
+  AddAdress({super.key});
+  final _formKey = GlobalKey<FormState>();
+  TextEditingController FullAdress = TextEditingController();
+  TextEditingController PinCode = TextEditingController();
+  TextEditingController HouseNumber = TextEditingController();
+  TextEditingController FloorNumber = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Theme.of(context).colorScheme.background,
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Text(
+                "Enter Complete Address",
+                style: Theme.of(context)
+                    .textTheme
+                    .titleLarge!
+                    .copyWith(fontWeight: FontWeight.bold),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Column(
+                      children: [
+                        TextFormFieldMethod(context, FullAdress,
+                            "Enter Full Address *", Icons.home),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Column(
+                      children: [
+                        TextFormFieldMethod(
+                            context, PinCode, "Pincode *", Icons.post_add),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Column(
+                      children: [
+                        TextFormFieldMethod(context, HouseNumber,
+                            "House Number (Optional)", Icons.post_add),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Column(
+                      children: [
+                        TextFormFieldMethod(context, FloorNumber,
+                            "Floor Number (Optional)", Icons.post_add),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    ElevatedButton(
+                      onPressed: () {},
+                      child: const Text("Submit"),
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(
+                              20.0), // Set the border radius value
+                        ),
+                        backgroundColor: Theme.of(context).colorScheme.primary,
+                        padding: const EdgeInsets.symmetric(horizontal: 100.0),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget TextFormFieldMethod(
+      BuildContext context, controllerDetails, label, iconDetails) {
+    return TextFormField(
+      controller: controllerDetails,
+      obscureText: false,
+      //keyboardType: TextInputType.number,
+      decoration: InputDecoration(
+        labelText: label,
+        border: OutlineInputBorder(borderRadius: BorderRadius.circular(20)),
+        prefixIcon: Icon(
+          iconDetails,
+          color: Theme.of(context).colorScheme.primary,
+        ),
+      ),
+    );
+  }
+}
--- lib/src/screens/userAdress/initial_adress.dart
+++ lib/src/screens/userAdress/initial_adress.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:sample_application/src/global_service/global_service.dart';
+import 'package:sample_application/src/screens/userAdress/add_address.dart';
 
 class InitialAdress extends StatefulWidget {
   const InitialAdress({super.key});
@@ -8,8 +10,35 @@ class InitialAdress extends StatefulWidget {
 }
 
 class _InitialAdressState extends State<InitialAdress> {
+  GlobalService globalservice = GlobalService();
+  List<String> items = List.generate(18,
+      (index) => '4/5, 1st cross road, Byrappa Layout, whitefield, Bangalore');
+
+  int visibleItemCount = 5;
+
+  void _loadMoreItems() {
+    setState(() {
+      if (visibleItemCount < items.length) {
+        visibleItemCount += 5;
+      } else {
+        visibleItemCount = visibleItemCount;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
+    bool isButtonEnabled = false;
+    bool isItemsNotPresent = false;
+    if (items.length == 0) {
+      isItemsNotPresent = true;
+    }
+    if (items.length < visibleItemCount) {
+      visibleItemCount = items.length;
+    }
+    if (visibleItemCount < items.length) {
+      isButtonEnabled = true;
+    }
     return SafeArea(
       child: Scaffold(
         body: SingleChildScrollView(
@@ -46,43 +75,13 @@ class _InitialAdressState extends State<InitialAdress> {
               SizedBox(
                 height: 10,
               ),
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: Row(
-                  children: [
-                    Expanded(
-                      child: Container(
-                        width: 250,
-                        child: TextField(
-                          decoration: InputDecoration(
-                            hintText: "Enter Pin Code",
-                            contentPadding: EdgeInsets.all(16),
-                            border: OutlineInputBorder(
-                                borderRadius:
-                                    BorderRadius.all(Radius.circular(10))),
-                          ),
-                        ),
-                      ),
-                    ),
-                    OutlinedButton(
-                      onPressed: () {},
-                      child: Text("Check"),
-                      style: OutlinedButton.styleFrom(
-                        shape: RoundedRectangleBorder(
-                          borderRadius:
-                              BorderRadius.circular(10), // Set border radius
-                        ),
-                        // Set border properties
-                      ),
-                    )
-                  ],
-                ),
-              ),
               Divider(
                 height: 3,
               ),
               InkWell(
-                onTap: () {},
+                onTap: () {
+                  Navigator.pop(context);
+                },
                 child: Container(
                   alignment: Alignment.bottomLeft,
                   child: Padding(
@@ -117,7 +116,9 @@ class _InitialAdressState extends State<InitialAdress> {
                 height: 5,
               ),
               InkWell(
-                onTap: () {},
+                onTap: () {
+                  globalservice.navigate(context, AddAdress());
+                },
                 child: Container(
                   alignment: Alignment.bottomLeft,
                   child: Padding(
@@ -151,6 +152,90 @@ class _InitialAdressState extends State<InitialAdress> {
               Divider(
                 height: 5,
               ),
+              SizedBox(
+                height: 20,
+              ),
+              Container(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                  child: Text(
+                    "Saved addresses",
+                    style: Theme.of(context)
+                        .textTheme
+                        .titleLarge!
+                        .copyWith(fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              isItemsNotPresent
+                  ? Column(
+                      children: [
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Text(
+                          "No Saved Address",
+                          style: Theme.of(context).textTheme.titleLarge,
+                        ),
+                        Divider(
+                          height: 20,
+                        )
+                      ],
+                    )
+                  : SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.6,
+                      width: MediaQuery.of(context).size.width * 0.9,
+                      child: ListView.builder(
+                        itemCount: visibleItemCount +
+                            1, // Add 1 for the "Load More" button
+                        itemBuilder: (context, index) {
+                          if (index < visibleItemCount) {
+                            return InkWell(
+                              onTap: () {},
+                              child: Container(
+                                child: Column(
+                                  children: [
+                                    Row(
+                                      children: [
+                                        SizedBox(width: 10),
+                                        Icon(
+                                          Icons.home,
+                                          color: Theme.of(context)
+                                              .colorScheme
+                                              .primary,
+                                        ),
+                                        SizedBox(
+                                          width: 10,
+                                        ),
+                                        Expanded(
+                                          child: ListTile(
+                                            title: Text(items[index]),
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                    Divider(
+                                      height: 10,
+                                    )
+                                  ],
+                                ),
+                              ),
+                            );
+                          } else {
+                            return isButtonEnabled
+                                ? ElevatedButton(
+                                    onPressed: _loadMoreItems,
+                                    child: Text('Load More'),
+                                  )
+                                : null;
+                          }
+                        },
+                      ),
+                    )
             ],
           ),
         ),
